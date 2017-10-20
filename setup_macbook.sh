500e38ec4b83f46d4fdd7be64bb48d0f845cae59
king0120 <king0120@gmail.com>
2017-10-20 12:34:39 -0400

    added bash_it and brew services

--- setup_macbook.sh
+++ setup_macbook.sh
@@ -10,3 +10,19 @@ echo "Brew Libraries Installed"
 brew cask install flycut google-chrome iterm2 slack spectacle visual-studio-code
 
 echo "Installed Development Programs"
+
+brew services start mongodb
+brew services start postgres
+
+echo "Brew Services Start Complete"
+
+echo "Installing Bash-it"
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh --silent
+echo "Bash-It Complete"
+
+echo "Add Code Script to Bash Profile"
+cat << EOF >> ~/.bash_profile
+# Add Visual Studio Code (code)
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+EOF
